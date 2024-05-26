a6d31dd73aad693f92486075bfe5698e4e4161ce
Yangdaehan <yangdh08@naver.com>
2024-05-26 18:22:06 +0900

    [FEAT] : domain으로 ip주소 찾기 기능
    domain으로 ip 주소 찾기 기능 추가

---
+++ RootDnsServer/src/main/java/org/mse/rootdnsserver/model/DnsEntry.java
@@ -0,0 +1,22 @@
+package org.mse.rootdnsserver.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DnsEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String domain;
+    private String ipAddress;
+
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+}
---
+++ RootDnsServer/src/main/java/org/mse/rootdnsserver/repository/DnsRepository.java
@@ -0,0 +1,11 @@
+package org.mse.rootdnsserver.repository;
+
+
+import java.util.Optional;
+import org.mse.rootdnsserver.model.DnsEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DnsRepository extends JpaRepository<DnsEntry, Long> {
+    Optional<DnsEntry> findByDomain(String domain);
+}
+
