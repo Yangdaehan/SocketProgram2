43bb66805d08a4399d63d53e7a545cf1a241dd97
Yangdaehan <yangdh08@naver.com>
2024-05-26 18:32:23 +0900

    [FEAT] : Local Dns 서버에서 Root Dns 서버 연결
    Local Dns 서버에서 해당 도메인을 못 찾을 경우 Root Dns 서버로 통신 후 도메인 찾기 기능 구현

--- LocalDnsServer/src/main/java/org/mse/localdnsserver/model/DnsEntry.java
+++ LocalDnsServer/src/main/java/org/mse/localdnsserver/model/DnsEntry.java
@@ -14,6 +14,25 @@ public class DnsEntry {
     private String domain;
     private String ipAddress;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 
     public String getIpAddress() {
         return ipAddress;
--- LocalDnsServer/src/main/java/org/mse/localdnsserver/service/DnsService.java
+++ LocalDnsServer/src/main/java/org/mse/localdnsserver/service/DnsService.java
@@ -4,14 +4,16 @@ import org.mse.localdnsserver.model.DnsEntry;
 import org.mse.localdnsserver.repository.DnsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DnsService {
 
-    private static final Logger logger = LoggerFactory.getLogger(DnsService.class);
+   /* private static final Logger logger = LoggerFactory.getLogger(DnsService.class);
     private final DnsRepository dnsRepository;
 
     public DnsService(DnsRepository dnsRepository) {
@@ -29,6 +31,33 @@ public class DnsService {
             logger.warn("Domain {} not found", domain);
             return Optional.empty();
         }
+    }*/
+   @Autowired
+   private DnsRepository dnsRepository;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final String rootDnsServerUrl = "http://localhost:8081/dns/resolve?domain=";
+
+    public Optional<String> resolveDomain(String domain) {
+        Optional<DnsEntry> dnsEntryOptional = dnsRepository.findByDomain(domain);
+        if (dnsEntryOptional.isPresent()) {
+            return Optional.of(dnsEntryOptional.get().getIpAddress());
+        } else {
+            String rootDnsUrl = rootDnsServerUrl + domain;
+            String ipAddress = restTemplate.getForObject(rootDnsUrl, String.class);
+            if (ipAddress != null) {
+                DnsEntry newDnsEntry = new DnsEntry();
+                newDnsEntry.setDomain(domain);
+                newDnsEntry.setIpAddress(ipAddress);
+                dnsRepository.save(newDnsEntry);
+                return Optional.of(ipAddress);
+            } else {
+                return Optional.empty();
+            }
+        }
     }
+
+
 
 }
