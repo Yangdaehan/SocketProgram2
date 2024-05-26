b6a3c2824f13af1cab3deaadc8b0a6eb568d6c53
Yangdaehan <yangdh08@naver.com>
2024-05-26 18:02:40 +0900

    [FEAT] : Root Dns 서버 구현
    Root DNS 서버 구현

---
+++ RootDnsServer/src/main/java/org/mse/rootdnsserver/RootDnsServerApplication.java
@@ -0,0 +1,13 @@
+package org.mse.rootdnsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootDnsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootDnsServerApplication.class, args);
+    }
+
+}
---
+++ RootDnsServer/src/test/java/org/mse/rootdnsserver/RootDnsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mse.rootdnsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootDnsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
